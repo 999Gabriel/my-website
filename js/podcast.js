fe519c4d9a8e6042104271b8e450dca9503e22c1
999Gabriel <999gabriel.winkler@gmail.com>
2024-11-22 12:05:43 +0100

    small details added

--- js/podcast.js
+++ js/podcast.js
@@ -44,3 +44,21 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.navbar nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
